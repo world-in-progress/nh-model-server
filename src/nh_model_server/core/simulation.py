6217fa90e6520aba6db3f6a923f448ee9b2f631a
Stevnda <1849698643@qq.com>
2025-07-04 10:20:50 +0800

    Refactor SimulationProcessManager to manage multiple processes for simulations, enhancing process handling and monitoring capabilities.

--- src/nh_model_server/core/simulation.py
+++ src/nh_model_server/core/simulation.py
@@ -15,70 +15,63 @@ class SimulationProcessManager:
     def start(self, solution_name, simulation_name, solution_data, resource_path, simulation_address=None, step=None):
         key = self._get_key(solution_name, simulation_name)
         with self.lock:
-            if key in self.processes and self.processes[key].poll() is None:
-                return False  # 该任务已在运行
-            
-            # 启动模拟进程
-            args = ['python', 'test/example_model_entry.py', solution_name, simulation_name, solution_data, resource_path, step]
-            if step is not None:
-                args.append(str(step))
-            proc = subprocess.Popen(args)
-            self.processes[key] = proc
-            
+            if key in self.processes:
+                procs = self.processes[key]
+                if all(proc.poll() is None for proc in procs.values()):
+                    return False  # 该任务已在运行
+
+            # 启动两个进程
+            # flood_proc = multiprocessing.Process(
+            #     target=run_flood,
+            #     args=(solution_name, simulation_name, solution_data, resource_path, step)
+            # )
+            # pipe_proc = multiprocessing.Process(
+            #     target=run_pipe_simulation,
+            #     args=(solution_name, simulation_name, solution_data, resource_path, step)
+            # )
+            # flood_proc.start()
+            # pipe_proc.start()
+            # self.processes[key] = {'flood': flood_proc, 'pipe': pipe_proc}
+
             # 启动结果监控器
             if simulation_address:
                 monitor = ResultMonitor(resource_path, simulation_address, solution_name, simulation_name)
                 monitor.start()
                 self.monitors[key] = monitor
-            
-            # mp_proc = multiprocessing.Process(
-            #     target=run_simulation,
-            #     args=(solution_name, simulation_name, solution_data, resource_path, step)
-            # )
-            # mp_proc.start()
-            # self.processes[key] = mp_proc
-            
+
             return True
 
     def stop(self, solution_name, simulation_name):
         key = self._get_key(solution_name, simulation_name)
         with self.lock:
-            # 停止模拟进程
-            proc = self.processes.get(key)
-            if proc and proc.poll() is None:
-                proc.terminate()
-                proc.wait()
+            # 停止模拟进程组
+            procs = self.processes.get(key)
+            if procs:
+                for proc in procs.values():
+                    if proc.is_alive():
+                        proc.terminate()
+                        proc.join()
                 del self.processes[key]
-            # # 备用：如果使用 multiprocessing.Process 启动
-            # if proc and proc.is_alive():
-            #     proc.terminate()
-            #     proc.join()
-            #     del self.processes[key]
-            
+
             # 停止监控器
             monitor = self.monitors.get(key)
             if monitor:
                 monitor.stop()
                 del self.monitors[key]
-            
+
             return True
 
     def stop_all(self):
         """停止所有进程和监控器"""
         with self.lock:
-            # 停止所有进程
-            for key, proc in list(self.processes.items()):
-                if proc and proc.poll() is None:
-                    proc.terminate()
-                    proc.wait()
+            # 停止所有进程组
+            for key, procs in list(self.processes.items()):
+                for proc in procs.values():
+                    if proc.is_alive():
+                        proc.terminate()
+                        proc.join()
             self.processes.clear()
-            # # 备用：如果使用 multiprocessing.Process 启动
-            # for key, proc in list(self.processes.items()):
-            #     if proc and proc.is_alive():
-            #         proc.terminate()
-            #         proc.join()
-            # self.processes.clear()
-            
+
             # 停止所有监控器
             for key, monitor in list(self.monitors.items()):
                 monitor.stop()
